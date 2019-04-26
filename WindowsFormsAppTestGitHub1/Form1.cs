f783931b36e3b16ed1f58a495b13dcfbbee495a6
Lorenzo Guida <lorenzo.guida@gmail.com>
2019-04-26 17:16:45 +0200

    New

--- WindowsFormsAppTestGitHub1/Form1.cs
+++ WindowsFormsAppTestGitHub1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsAppTestGitHub1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Ciao");
+            MessageBox.Show("Ciao2");
         }
     }
 }
